ba1a60b97e27079b5b00eeebb3a43777e46d1679
wenguang <gwen@novasky.cn>
2020-03-20 11:53:14 +0800

    头像上传功能实现

--- src/main/java/com/we/controller/PhotographController.java
+++ src/main/java/com/we/controller/PhotographController.java
@@ -3,6 +3,19 @@ package com.we.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import sun.misc.BASE64Decoder;
+import sun.text.resources.FormatData;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.Format;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/photograph")
@@ -12,6 +25,40 @@ public class PhotographController {
     @GetMapping()
     public String file() {
         return prefix + "/photo";
+    }
+
+
+    /**
+     * 头像上传
+     * @param file
+     * @return
+     */
+    @RequestMapping("/photoUpload")
+    @ResponseBody
+    public String fileUpload(@RequestParam("op") String op, @RequestParam("base64url") String base64url) {
+        if (!"takePhoto".equals(op)) {
+            return "false";
+        }
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            // Base64解码
+            byte[] b = decoder.decodeBuffer(base64url);
+            for (int i = 0; i < b.length; ++i) {
+                if (b[i] < 0) {// 调整异常数据
+                    b[i] += 256;
+                }
+            }
+            String imgFilePath = "D:\\datafile\\photo.jpg";
+            OutputStream out = new FileOutputStream(imgFilePath);
+            out.write(b);
+            out.flush();
+            out.close();
+
+            return "true";
+        } catch (Exception e) {
+            return "false";
+        }
+
     }
 
 }
