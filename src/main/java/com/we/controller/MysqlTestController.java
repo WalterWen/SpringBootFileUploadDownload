7d57795e723db5723941061f1d5ac7ce72fecb4d
wenguang <gwen@novasky.cn>
2020-03-26 09:25:11 +0800

    bootstrap引入，展示学生信息表格

--- src/main/java/com/we/controller/MysqlTestController.java
+++ src/main/java/com/we/controller/MysqlTestController.java
@@ -1,12 +1,18 @@
 package com.we.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.mysql.cj.xdevapi.JsonArray;
 import com.we.pojo.Student;
 import com.we.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
@@ -18,13 +24,19 @@ public class MysqlTestController {
     @Autowired
     private IStudentService studentService;
 
-
     @GetMapping("/listtest")
     public String selectAll(Model model) {
         List<Student> stus = studentService.selectStuList();
-        //Map<String, Object> model= new HashMap<String, Object>();
-        //model.put("stus", stus);
         model.addAttribute("stus", stus);
         return prefix + "/listtest";
     }
+
+    @PostMapping("/list")
+    @ResponseBody
+    public JSONArray selectAllTable() {
+        List<Student> stus = studentService.selectStuList();
+        JSONArray array= JSONArray.parseArray(JSON.toJSONString(stus));
+        return array;
+    }
+
 }
